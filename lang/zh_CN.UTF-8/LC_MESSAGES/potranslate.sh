f52a54e4c926d05ea22ed0048c651a4c83dbc661
Aladdin Chan <aladdin.china@gmail.com>
2019-12-28 16:48:49 +0800

    Fix: When there are double quotes in the result, only partial result is returned.

--- lang/zh_CN.UTF-8/LC_MESSAGES/potranslate.sh
+++ lang/zh_CN.UTF-8/LC_MESSAGES/potranslate.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-#Simple po file translate script through google translate api v2.
-#Author : CHEN JUN<aladdin.china@gmai.com> , 2019
+
+# Simple po file translate script through google translate api v2.
+# Tested on macOS 10.15
+# Author : CHEN JUN<aladdin.china@gmai.com> , 2019
+
 POFILE_IN=$1
 POFILE_OUT=$2
-APIKEY="XXXXXXXXXXnYXRshXvPytqEPS1zXPZbXbNX2C9M" #replaced by your own google translate api key.
+APIKEY="XXXXXXXXXXnYXRshXvPytqEPS1zXPZbXbNX2C9M" # Replaced by your own google translate api key.
 SOURCELANG="en"
 TARGETLANG="zh-Hans"
 
 MSGID_START=False
 while read -r LINE; do
     if [[ "${LINE:0:5}" == "msgid" ]] ; then
-        #msgid may have multiple line.
+        # msgid may have multiple line.
         MSGID_START=True
-        MSGID=${LINE:7:${#LINE}-8} # message in double quotes.
+        MSGID=${LINE:7:${#LINE}-8} # Message in double quotes.
     elif [[ "${LINE:0:6}" == "msgstr" ]] ; then 
-        #one msgid readed. tranlate through google api.
+        # One msgid readed, tranlate through google api.
         if [[ "$MSGID" != "" ]] ; then
             MSGSTR=`curl -s -X POST \
                 "https://translation.googleapis.com/language/translate/v2?key=$APIKEY" \
@@ -30,7 +33,8 @@ while read -r LINE; do
                 echo "$MSGSTR"
                 exit 1
             fi
-            MSGSTR1=`echo $MSGSTR | grep translatedText | cut -d \" -f 8`
+            #MSGSTR1=`echo $MSGSTR | grep translatedText | cut -d \" -f 8` # When there are double quotes in the result, only partial result is returned.
+            MSGSTR1=`echo $MSGSTR | sed 's/.*translatedText":\ "\(.*\)"\ .*$/\1/g'`
             if [[ "$MSGSTR1" == "" ]] ; then
                 echo "Something wrong."
                 echo "$MSGSTR"
@@ -38,11 +42,11 @@ while read -r LINE; do
             fi
             MSGSTR="msgstr \"$MSGSTR1\""
         else 
-            MSGSTR="msgstr \"\""   #blank msgid.
+            MSGSTR="msgstr \"\""   # Blank msgid.
         fi
         echo "$MSGSTR" >> $POFILE_OUT
         
-        #next msgid
+        # Next msgid
         MSGID=""
         MSGID_START=False
         continue
@@ -54,7 +58,7 @@ while read -r LINE; do
             MSGID="$MSGID $MSGID1"
         fi
     fi
-    echo "$LINE" >> $POFILE_OUT   #double quotes is necessary. because msgid may having leading spaces.
+    echo "$LINE" >> $POFILE_OUT   # Double quotes is necessary, because msgid may having leading spaces.
 done < $POFILE_IN
 
 #End
